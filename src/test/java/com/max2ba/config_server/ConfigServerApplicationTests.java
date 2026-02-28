8cb4808198a1f1d9908638fa95a21daed3884773
MaxTsumentsau <max2ba@yandex.ru>
2026-02-28 14:32:18 +0300

    Initial commit

---
+++ src/test/java/com/max2ba/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max2ba.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
